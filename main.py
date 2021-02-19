0637ee632e0ad98ffcd0c974434179b4d7fcc5fb
Wister <e@wister.mx>
2021-02-19 00:54:49 -0600

    api init

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask import Flask, request, json, Response
+from pymongo import MongoClient
 
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def base():
+    return Response(response=json.dumps({"Status": "UP"}),
+                    status=200,
+                    mimetype='application/json')
+  
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host='0.0.0.0')
+
+
